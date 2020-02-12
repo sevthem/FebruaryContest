14df5f8cbc9d708a55200ee07a663e8c31636a37
Sevastos Themistoklis <sevthem@outlook.com>
2020-02-12 23:43:56 +0200

    Initial commit

--- FebruaryContestEntry/Program.cs
+++ FebruaryContestEntry/Program.cs
@@ -7,14 +7,37 @@ namespace FebruaryContestEntry
 		const char block = '▓';
 
 		static void Main(string[] args)
-		{
-			// The solution implementation goes here
+		{            
+            int x = 30, y = 15;
+            var matrix = new int[y, x];
 
-			Console.ResetColor();
+            for (int i = 1; i < y - 1; i++)
+            {
+                for (int j = 1; j < x - 1; j++)
+                {
+                    matrix[i, j] = 1;
+                }
+            }
+
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    Write(matrix[i, j] == 0 ? ConsoleColor.Green : ConsoleColor.Yellow);
+                }
+                Console.Write(Environment.NewLine);
+            }
+
+            Console.ResetColor();
 			Console.WriteLine();
 			Console.WriteLine();
 			Console.WriteLine("Yay, a colorful shape!");
 			Console.ReadLine();
 		}
-	}
+        private static void Write(ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(block);
+        }
+    }
 }
